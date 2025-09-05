f1daaed34f751c4627869bbd6c4397dfdd3bb414
azad-singh1390 <azadsingh1390@icloud.com>
2025-09-05 14:07:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ res.json(results);
 });
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
 console.log(`Server running on port ${PORT}`);
 });
