c0248190eeca13134fb014945b4f5421017b469f
Azad Singh Sandhu <azadsingh1390@icloud.com>
2025-09-06 02:26:10 +0530

    Updated Server

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const PORT = process.env.PORT || 8080;
 
 // MySQL connection
 const db = mysql.createConnection({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 db.connect(err => {
