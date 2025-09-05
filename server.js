a99c8d7264d441d8b0e249a9631d610705cdf332
Azad Singh Sandhu <azadsingh1390@icloud.com>
2025-09-05 14:17:17 +0530

    backend files

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
-require("dotenv").config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
 const app = express();
 app.use(cors());
