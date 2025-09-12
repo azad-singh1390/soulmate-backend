ce1648de7fc9e843e9ba503158a0781b95561e53
Azad Singh Sandhu <azadsingh1390@icloud.com>
2025-09-12 22:49:43 +0530

    Updating Files

--- server.js
+++ server.js
@@ -134,7 +134,7 @@ app.get("/comingbookings", async (req, res) => {
     const [rows] = await pool.query(`
       SELECT client_name, client_number, event_date, event_type, venue
       FROM bookings 
-      WHERE event_date BETWEEN CURDATE() AND DATE_ADD(CURDATE(), INTERVAL 2 DAY)
+      WHERE event_date BETWEEN CURDATE() AND DATE_ADD(CURDATE(), INTERVAL 7 DAY)
     `);
     res.json({ rows });
   } catch (err) {
