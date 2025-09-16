8d78121897741ed4612a347ef656da7d43434ef8
Azad Singh Sandhu <azadsingh1390@icloud.com>
2025-09-16 23:22:50 +0530

    Updating Files

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const PORT = process.env.PORT || 8080;
 app.use(cors({
   origin: ["http://127.0.0.1:5500", "https://azad-singh1390.github.io"],
   methods: ["GET", "POST", "DELETE", "PUT"],
-  allowedHeaders: ["Content-Type"]
+  allowedHeaders: ["Content-Type"],
+  credentials: true
 }));
 
 app.use(express.static(path.join(__dirname, 'public')));
